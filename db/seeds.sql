0ca05104fe0bc38536a4a05c55ef9c8fd8e44eb4
lana <lanabkim@gmail.com>
2021-01-14 23:14:35 -0800

    adding sql files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (name,isDevoured) VALUES ("THE Burger",0);
+INSERT INTO burgers (name,isDevoured) VALUES ("Jalapeno Burger",0);
+INSERT INTO burgers (name,isDevoured) VALUES ("Deep Cheese Burger",1);
+INSERT INTO burgers (name,isDevoured) VALUES ("Avocadict Burger",1);
+INSERT INTO burgers (name,isDevoured) VALUES ("Double Chili Burger",1);
+INSERT INTO burgers (name,isDevoured) VALUES ("Triple Cheese Burger",1);
+INSERT INTO burgers (name,isDevoured) VALUES ("Bacon and Muchroom Burger",1);
