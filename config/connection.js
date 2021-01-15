86878bd6828509895f80284c3d5b0bb4f57a9dab
lana <lanabkim@gmail.com>
2021-01-14 22:53:21 -0800

    server listening

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
 	host: "localhost",
 	user: "root",
 	password: process.env.DB_PASSWORD,
-	database: "employees_db",
+	database: "burger_db",
 	multipleStatements: true,
 });
 
--- server.js
+++ server.js
@@ -1,23 +1,47 @@
-const express = require("express");
+// const express = require("express");
 
-const PORT = process.env.PORT || 7856;
+// const PORT = process.env.PORT || 7856;
 
-const app = express();
+// const app = express();
+
+// app.use(express.static("public"));
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// const exphbs = require("express-handlebars");
+
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+
+// const routes = require("./controllers/controller.js");
+
+// app.use(routes);
+
+// app.listen(PORT, function () {
+// 	console.log("Server listening on: http://localhost:" + PORT);
+// });
+
+var express = require("express");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
 
 app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const exphbs = require("express-handlebars");
+var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-const routes = require("./controllers/controller.js");
+var routes = require("./controllers/controller.js");
 
 app.use(routes);
 
 app.listen(PORT, function () {
-	console.log("Server listening on: http://localhost:" + PORT);
+	console.log("App now listening at localhost:" + PORT);
 });
