64e48c6b4d999a7d62e5cbf04f64764971af26a1
lana <lanabkim@gmail.com>
2021-01-14 22:25:23 -0800

    connection.js added

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql");
+const dotenv = require("dotenv").config();
+const connection = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: process.env.DB_PASSWORD,
+	database: "employees_db",
+	multipleStatements: true,
+});
+
+connection.connect((err) => {
+	if (err) throw err;
+	console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
